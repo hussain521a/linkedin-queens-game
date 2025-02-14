4650ef0c04e9a1f53650a0fe980d70e278e3f2fb
Hussain Ali <hussain521ali@gmail.com>
2025-02-14 14:21:48 -0500

    Updated QueensWindow GUI and put window panels into methods Created enum to toggle cell States

---
+++ CellState.java
@@ -0,0 +1,6 @@
+//Cycles through the different cell states
+public enum CellState {
+    Normal,
+    X,
+    Queen
+}
--- QueensWindow.java
+++ QueensWindow.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.concurrent.Flow;
 
 /**
@@ -17,6 +19,12 @@ public class QueensWindow implements Runnable, ActionListener {
     private JButton resetButton;
     private int seconds = 0;
     private JLabel timerLabel;
+    private static final int BOARD_SIZE = 8;
+    private static final int CELL_SIZE = 50; 
+    private CellState currentDragState = CellState.Normal;
+    private JPanel leftPanel;
+    private JPanel boardPanel;
+    
 
     /**
      * 
@@ -47,58 +55,90 @@ public class QueensWindow implements Runnable, ActionListener {
 	
 	// create a JFrame where we build our GUI
 	JFrame frame = new JFrame("Queens");
-    frame.setPreferredSize(new Dimension(600,500));
+    //frame.setPreferredSize(new Dimension(700,600));
     //Application terminates when window closed
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLayout(new BorderLayout());
 
-    //Left side menu panel
-    JPanel menuPanel = new JPanel();
-    menuPanel.setLayout(new BorderLayout());
-    menuPanel.setPreferredSize(new Dimension(150,frame.getHeight()));
-    frame.add(menuPanel, BorderLayout.WEST);
-
-    //Create button panel to center in menu panel
-    JPanel buttonPanel = new JPanel();
-    buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-
-    //Create label for timer
-    JLabel timerLabel = new JLabel("Time: 00:00");
-    timerLabel.setFont(new Font("Serif", Font.BOLD, 20));
-
-    //Create start and reset buttons
-    startButton = new JButton("Start");
-    resetButton = new JButton("Reset");
-    startButton.addActionListener(this);
-    resetButton.addActionListener(this);
-
-    //Add buttons and label to button panel
-    buttonPanel.add(timerLabel, BorderLayout.NORTH);
-    buttonPanel.add(startButton);
-    buttonPanel.add(resetButton);
-
-    //Add button panel to menu panel
-    menuPanel.add(buttonPanel, BorderLayout.CENTER);
-    frame.add(menuPanel, BorderLayout.WEST);
-
-    //Right side board panel
-    JPanel boardPanel = new JPanel(new GridLayout(8, 8));
-    for(int i = 0; i < 64; i++) {
-        JButton button = new JButton();
-        button.addActionListener(this);
-        boardPanel.add(button);
-    }
-    frame.add(boardPanel, BorderLayout.CENTER);
+    createLeftPanel(frame);
+    createBoardPanel(frame);
+    
 
 	// display the window we've created
 	frame.pack();
 	frame.setVisible(true);
     }
-    
-    public static void main(String args[]) {
 
-	// The main method will construct and show the GUI
-	javax.swing.SwingUtilities.invokeLater(new QueensWindow());
+    private void createLeftPanel(JFrame frame) {
+        //Left side menu panel
+        leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        leftPanel.setPreferredSize(new Dimension(300,frame.getHeight()));
+        frame.add(leftPanel, BorderLayout.WEST);
+
+        //Create header label for menu panel
+        JLabel menuLabel = new JLabel("Queens");
+        menuLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        leftPanel.add(menuLabel);
+        menuLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        leftPanel.add(Box.createVerticalStrut(30));
+        leftPanel.add(menuLabel);
+
+        //Create panel to hold timer
+        JPanel timerPanel = new JPanel();
+        timerPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        //timerPanel.setBackground(Color.WHITE);
+
+        //Create label for timer
+        JLabel timerLabel = new JLabel("Time: 00:00");
+        timerLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        timerPanel.add(timerLabel);
+
+        //Add timer panel to left panel
+        leftPanel.add(Box.createVerticalStrut(100));
+        leftPanel.add(timerPanel);
+
+
+        //Create button panel to hold all buttons
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10)); //horizontal and vertical padding
+
+
+        //Create start and reset buttons
+        startButton = new JButton("Start");
+        resetButton = new JButton("Reset");
+        startButton.addActionListener(this);
+        resetButton.addActionListener(this);
+
+        //Add buttons and label to button panel
+        buttonPanel.add(startButton);
+        buttonPanel.add(resetButton);
+
+        //Add button panel to menu panel
+        leftPanel.add(Box.createVerticalGlue());
+        leftPanel.add(buttonPanel);
+        leftPanel.add(Box.createVerticalStrut(30));
+
+        frame.add(leftPanel, BorderLayout.WEST);
+    }
+    
+    private void createBoardPanel(JFrame frame) {
+        //Right side board panel
+        boardPanel = new JPanel(new GridLayout(BOARD_SIZE, BOARD_SIZE));
+        boardPanel.setPreferredSize(new Dimension(BOARD_SIZE * CELL_SIZE, BOARD_SIZE * CELL_SIZE));
+        for(int row = 0; row < BOARD_SIZE; row++) {
+            for(int col = 0; col < BOARD_SIZE; col++) {
+                JButton button = new JButton();
+                button.addActionListener(this);
+                boardPanel.add(button);
+            }
+        }
+    frame.add(boardPanel, BorderLayout.CENTER);
+    }
+
+    public static void main(String args[]) {
+        // The main method will construct and show the GUI
+        javax.swing.SwingUtilities.invokeLater(new QueensWindow());
     }
 }
    
